08d290f930dd05ccbc167be8552dfa2e6f4df91d
iaSalazar <ia.salazar@uniandes.edu.co>
2022-02-11 00:27:07 -0500

    .

--- flask-server/controller.py
+++ flask-server/controller.py
@@ -4,16 +4,10 @@ from models import User, Contest, user_schema, contest_schema
 
 import datetime
 from flask import request, jsonify
-#from flask_sqlalchemy import SQLAlchemy 
-#from flask_marshmallow import Marshmallow  
-#from flask_restful import Api, Resource 
 
-#from flask_jwt_extended import create_access_token
-#from flask_jwt_extended import get_jwt_identity
-#from flask_jwt_extended import jwt_required
-#from flask_jwt_extended import JWTManager
 import flask_praetorian
 #import flask_cors
+
 
 
 
