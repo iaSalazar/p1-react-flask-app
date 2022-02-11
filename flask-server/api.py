6e802edbe805689346c2e8d823302c96fe80b0da
iaSalazar <ia.salazar@uniandes.edu.co>
2022-02-11 00:37:24 -0500

    removed unused files and modules

--- flask-server/api.py
+++ flask-server/api.py
@@ -1,31 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow  
-#from flask_restful import Api, Resource 
-
 
 
 
 app = Flask(__name__)#, static_folder="../build", static_url_path="/")
 
-#app.config["JWT_SECRET_KEY"] = "super-secret"
+
 app.secret_key = "super-secret"
 
-#app.config['JWT_IDENTITY_CLAIM'] = 'sub'
+
 app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
 app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# Setup the Flask-JWT-Extended extension
+
 
 
 
 db = SQLAlchemy(app) 
 ma = Marshmallow(app)
 
-#api = Api(app)
-db.init_app(app)
 
-# It must be done after declaring app adn db
+db.init_app(app)
 
 from models import *
 from controller import *
