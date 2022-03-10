a5a69efdd0d0e53f6d73674d4c9910294e6432f5
Luis Perez <le.perezl@uniandes.edu.co>
2022-03-09 23:04:51 -0500

    banner works

--- src/VoiceHome.js
+++ src/VoiceHome.js
@@ -2,24 +2,36 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import VoiceList from './VoiceList'
 import VoiceForm from './VoiceForm'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 
 
 function VoiceHome(props) {
     const [click, setClick] = useState(1)
     const [infoClick, setInfoClick] = useState(1)
+
     
     const handleClick = () => setClick(!click)
     const handleInfoClick = () => setInfoClick(!infoClick)
+    const {id_contest, url_contest} = useParams()
 
-    const {id_contest} = useParams()
+    let bannerLink=`/api/contests/${id_contest}/${url_contest}/banner`
+    const [banner, setBanner] = useState()
+
+    useEffect(()=>{
+      fetch(bannerLink).then((res) => res.blob()).then((data)=>{
+          var objectURL = URL.createObjectURL(data)
+          setBanner(objectURL)
+      })
+  
+    },[])
 //{display:flex; align-items:left padding:1rem}
 //style='display:inline-block  jus'
   return (
     
     <div>
-        <p>Concurso</p>
+        <p>Concurso {url_contest}</p>
+        <img src={banner} height="200"></img>
         <div >
             <Button variant="primary" onClick={handleClick}> Agregar</Button> <span>    </span>
             <Button variant="primary" onClick={handleClick} > Info</Button>
